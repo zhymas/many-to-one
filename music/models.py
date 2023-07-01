e301fe59b2936c46839fd1a7e8890aa166e248d4
dima <shvachko319@gmail.com>
2023-07-01 03:52:04 +0300

    new edits

--- music/models.py
+++ music/models.py
@@ -42,26 +42,5 @@ class Article(models.Model):
 
 
 
-# class Musician(models.Model):
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     instrument = models.CharField(max_length=100)
-#
-#
-# class Album(models.Model):
-#     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=100)
-#     release_date = models.DateField()
-#     num_stars = models.IntegerField()
 
-
-# class Person(models.Model):
-#     SHIRT_SIZE = (
-#         ('S', 'Small'),
-#         ('M', 'Medium'),
-#         ('L', 'Large'),
-#     )
-#     name = models.CharField(max_length=100)
-#     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZE)
-    # p.get_shirt_size_display()
 
